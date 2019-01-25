af92209c125ea5827bc4c5e4ecf10f15f19b6ebb
wuhk <wuhoukan@126.com>
2019-01-25 15:23:03 +0800

    [goods.vue]  revise core

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -15,9 +15,9 @@
         <li v-for="(item,index) in goods" :key="index" class="food-list">
           <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="(food,index) in item.foods" :key="index" class="item-food">
+            <li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px">
               <div class="icon">
-                <img :src="food.icon">
+                <img :src="food.icon" width="57px" height="57px">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
@@ -68,7 +68,7 @@ export default {
   .goods
     display: flex
     position: absolute
-    top: 174px
+    top: 179px
     bottom: 46px
     width: 100%
     overflow: hidden
@@ -77,10 +77,9 @@ export default {
       width: 80px
       background: #f3f5f7
       .menu-item
-        display: table
+        display: flex
         height: 54px
-        width: 56px
-        line-height: 14px
+        width: 100%
         .icon
           display: inline-block
           width: 12px
@@ -102,10 +101,50 @@ export default {
             bg-image('special_3')
         .text
           font-size: 12px
-          display: table-cell
-          width: 56px
-          right: 24px
+          width: 100%
+          display: flex
+          justify-content: center
+          align-items: center
+          list-style-type: none
           border-1px(rgba(7,17,27,0.1))
     .foods-wrapper
       flex: 1
+      .food-list,.item-food
+        list-style-type: none
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9ddb1
+        font-size: 12px
+        color: rgb(147,153,159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          felx: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+          .desc,.extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147,153,159)
+          .desc
+            margin-bottom: 8px
+          .extra
+            &.content
+              margin-right: 12px
+
 </style>
