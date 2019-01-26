2cf0ba3dde338a55be331a2d5edb5c6888a8f491
wuhk <wuhoukan@126.com>
2019-01-27 00:28:46 +0800

    [shopcart] creat component

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo">
+            <i class="icon-shopping_cart"></i>
+          </div>
+        </div>
+        <div class="price">¥0元</div>
+        <div class="desc"></div>
+      </div>
+      <div class="content-right"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="stylus">
+@import "~common/stylus/mixin"
+@import "~common/stylus/variable"
+.shopcart
+  position: fixed
+  left: 0
+  bottom: 0
+  z-index: 50
+  width: 100%
+  height: 48px
+  .content
+    display: flex
+    background: #141d27
+    font-size: 0
+    .content-left
+      flex: 1
+      .logo-wrapper
+        display: inline-block
+        position: relative
+        top: -10px
+        margin: 0 12px
+        padding: 6px
+        width: 56px
+        height: 56px
+        box-sizing: border-box
+        vertical-align: top
+        border-radius: 50%
+        background:  #141d27
+        .logo
+          width: 100%
+          height: 100%
+          border-radius: 50%
+          text-align: center
+          background: #2b343c
+          .icon-shopping_cart
+            font-size: 24px
+            line-height: 44px
+            color: #80858a
+      .price
+        display: inline-block
+        vertical-align: top
+        line-height: 24px
+        margin-top: 12px
+        padding-right: 12px
+        border-right: 1 solid rgba(255,255,255,0.4)
+        font-size: 16px
+        font-weight: 700
+        color: rgba(255,255,255,0.4)
+      .desc
+        display: inline-block
+    .content-right
+      flex: 0 0 105px
+      width: 105px
+</style>
