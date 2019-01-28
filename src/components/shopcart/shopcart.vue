32234348f2803cf534ec78d3a9d7f094d8dd0425
wuhk <wuhoukan@126.com>
2019-01-28 16:09:09 +0800

    [shopcart] add core

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,19 +3,59 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">¥0元</div>
-        <div class="desc"></div>
+        <div class="price" :class="{'highLight':totalPrice>0}">¥{{totalPrice}}元</div>
+        <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
       </div>
-      <div class="content-right"></div>
+      <div class="content-right">
+        <div class="pay">¥{{minPrice}}配送</div>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
+  props: {
+    selectFoods: {
+      type: Array,
+      default () {
+        return [
+          {
+            price: 1,
+            count: 1
+          }
+        ]
+      }
+    },
+    deliveryPrice: {
+      type: Number,
+      default: 0
+    },
+    minPrice: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    totalPrice () {
+      let total = 0
+      this.selectFoods.forEach((food) => {
+        total += food.price * food.count
+      })
+      return total
+    },
+    totalCount () {
+      let count = 0
+      this.selectFoods.forEach((food) => {
+        count += food.count
+      })
+      return count
+    }
+  }
 }
 </script>
 <style lang="stylus">
@@ -32,6 +72,7 @@ export default {
     display: flex
     background: #141d27
     font-size: 0
+    color: rgba(255,255,255,0.4)
     .content-left
       flex: 1
       .logo-wrapper
@@ -52,10 +93,28 @@ export default {
           border-radius: 50%
           text-align: center
           background: #2b343c
+          &.highlight
+            rgb(0,160,220)
           .icon-shopping_cart
             font-size: 24px
             line-height: 44px
             color: #80858a
+            &.highlight
+              color: #fff
+        .num
+          position: absolute
+          top: 0
+          right: 0
+          width: 24px
+          height: 16px
+          line-height: 16px
+          text-align:  center
+          font-size: 9px
+          border-radius: 16px
+          font-weight: 700
+          color: white
+          background: red
+          box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4)
       .price
         display: inline-block
         vertical-align: top
@@ -65,10 +124,21 @@ export default {
         border-right: 1 solid rgba(255,255,255,0.4)
         font-size: 16px
         font-weight: 700
-        color: rgba(255,255,255,0.4)
+        &.highlight
+          color: white
       .desc
         display: inline-block
+        vertical-align: top
+        line-height: 24px
+        margin: 12px 0 0 12px
+        font-size: 10px
     .content-right
       flex: 0 0 105px
       width: 105px
+      .pay
+        height: 48px
+        line-height: 48px
+        text-align: center
+        font-size: 12px
+        font-weight: 700
 </style>
