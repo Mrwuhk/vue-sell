a4b48e185aca4e40328e18c1bf8daa36b13c2294
wuhk <wuhoukan@126.com>
2019-01-30 03:51:21 +0800

    [core] SEO core

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0"></div>
+    <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart" transition="move">
+      <span class="inner icon-remove_circle_outline"></span>
+    </div>
     <div class="cart-count"  v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart"></div>
   </div>
@@ -16,8 +18,12 @@ export default {
   created () {
   },
   methods: {
+    decreaseCart () {
+      if (this.food.count > 0) {
+        this.food.count--
+      }
+    },
     addCart () {
-      console.log('click')
       if (!this.food.count > 0) {
         Vue.set(this.food, 'count', 1)
       } else {
@@ -32,19 +38,35 @@ export default {
 @import "~common/stylus/variable"
   .cartcontrol
     font-size: 0
-    .cart-decrease, .cart-add
+    .cart-decrease
       display: inline-block
       padding: 6px
-      line-height: 24px
-      font-size: 24px
-      color: rgb(0,160,220)
+      transition: all 0.4s linear
+      &.move-transition
+        opacity: 1
+        transform: translate3D(0,0,0)
+      .inner
+        display: inline-block
+        line-height: 24px
+        font-size: 24px
+        color: rgb(0,160,220)
+        transition: all 0.4s linear
+      &.move-enter,&move-leave
+        opacity: 0
+        transform: translate3D(24px,0,0)
     .cart-count
       display: inline-block
       font-size: 10px
       vertical-align: top
       width: 12px
-      padding-top: 6px
+      padding-top: 12px
       line-height: 10px
       text-align: center
-      color: gray   
+      color: gray
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0,160,220)
 </style>
